45a77766d671e4011d8a8545145ef954a16796c8
kmoussa <konemoussamohamed@gmail.com>
2022-03-19 03:43:39 +0100

    EtudiantControleur pour les operations de bases

--- src/main/java/projet/group2/gestionEmargement/controleur/EtudiantControleur.java
+++ src/main/java/projet/group2/gestionEmargement/controleur/EtudiantControleur.java
@@ -7,18 +7,38 @@ import projet.group2.gestionEmargement.dto.EtudiantDto;
 import projet.group2.gestionEmargement.entite.Etudiant;
 import projet.group2.gestionEmargement.service.EtudiantService;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(name = "/api")
 public class EtudiantControleur {
 
-//    @Autowired
-//    private EtudiantService etudiantService;
-//
-//    @PostMapping ("/etudiant")
-//    ResponseEntity<EtudiantDto> inscriptionEtudiant(@RequestBody EtudiantDto etudiantDto) {
-//        return etudiantService.inscriptionEtudiant();
-//
-//    }
+    @Autowired
+    private EtudiantService etudiantService;
 
+    @PostMapping("/creerEtudiant")
+    public Etudiant createStudent(@RequestBody Etudiant etudiant){
+        return etudiantService.creerEtudiant(etudiant);
+    }
 
+    @GetMapping("/getById/{id}")
+    public Etudiant getStudentById(@PathVariable String id){
+        return etudiantService.getEtudiantById(id);
+    }
+
+    @GetMapping("/all")
+    public List<Etudiant> getAllStudents(){
+        return etudiantService.getAllEtudiants();
+    }
+
+    @PutMapping("/update")
+    public Etudiant updateStudent(@RequestBody Etudiant etudiant){
+        return etudiantService.majEtudiant(etudiant);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteStudent(@PathVariable String id){
+        return etudiantService.supprimerEtudiant(id);
+    }
 }
+
