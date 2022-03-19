e994694f6eff7c8d98810df92dc0e84fe4f87764
kmoussa <konemoussamohamed@gmail.com>
2022-03-19 03:38:02 +0100

    EtudiantService pour les operations de bases

--- src/main/java/projet/group2/gestionEmargement/service/EtudiantService.java
+++ src/main/java/projet/group2/gestionEmargement/service/EtudiantService.java
@@ -5,14 +5,31 @@ import org.springframework.stereotype.Service;
 import projet.group2.gestionEmargement.entite.Etudiant;
 import projet.group2.gestionEmargement.repository.EtudiantRepository;
 
+import java.util.List;
+
 @Service
 public class EtudiantService {
-
     @Autowired
-    private EtudiantRepository etudiantRepository;
+    EtudiantRepository etudiantRepository;
 
-    public Etudiant inscriptionEtudiant(Etudiant etudiant) {
-        return this.etudiantRepository.save(etudiant);
+    public Etudiant creerEtudiant(Etudiant etudiant){
+        return etudiantRepository.save(etudiant);
     }
 
+    public Etudiant getEtudiantById(String id) {
+        return etudiantRepository.findById(id).get();
+    }
+
+    public List<Etudiant> getAllEtudiants() {
+        return etudiantRepository.findAll();
+    }
+
+    public Etudiant majEtudiant(Etudiant etudiant) {
+        return etudiantRepository.save(etudiant);
+    }
+
+    public String supprimerEtudiant(String id) {
+        etudiantRepository.deleteById(id);
+        return "Student has been deleted.";
+    }
 }
