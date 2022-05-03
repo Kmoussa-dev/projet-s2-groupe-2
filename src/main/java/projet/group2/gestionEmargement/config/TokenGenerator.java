46f3e529fcf835d0b127fa8f6bc70b3068028dd4
o22009067 <solomon.lourdessamy@etu.univ-orleans.fr>
2022-05-03 04:00:41 +0200

    ctrl d'intégrité par le moyen du QR code est à notre disposition 'mini refactorisation'

--- src/main/java/projet/group2/gestionEmargement/config/TokenGenerator.java
+++ src/main/java/projet/group2/gestionEmargement/config/TokenGenerator.java
@@ -1,6 +1,7 @@
 package projet.group2.gestionEmargement.config;
 
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
@@ -8,8 +9,11 @@ import com.google.zxing.qrcode.QRCodeWriter;
 import java.awt.image.BufferedImage;
 
 public class TokenGenerator {
-    public static BufferedImage generateQRCode(String uri) throws Exception{
-        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+    private TokenGenerator(){}
+
+    private static final QRCodeWriter qrCodeWriter = new QRCodeWriter();
+
+    public static BufferedImage generateQRCode(String uri) throws WriterException {
         BitMatrix bitMatrix = qrCodeWriter.encode(uri, BarcodeFormat.QR_CODE, 200,200);
         return MatrixToImageWriter.toBufferedImage(bitMatrix);
     }
